4896ce5e0a0af5053477f7a0a8acf290327837fe
George Magpie <prich845@gmail.com>
2019-03-16 20:29:10 +0300

    1.5. Checkstyle[#120727]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -12,8 +12,8 @@ public class Calculate {
 	* 
 	*/
 
-	public static void main (String [] args){
-				System.out.println ("Hello World!!");
+	public static void main(String[] args) {
+				System.out.println("Hello World!!");
 	}
 	
 	/**
