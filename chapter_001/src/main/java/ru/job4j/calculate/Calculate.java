0ce88d29d7369afe5c7bfae44cf47f48bdec4cbe
George Magpie <prich845@gmail.com>
2019-03-16 14:35:39 +0300

    1.3. JavaDoc. Code convention[#120725]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,21 @@
 package ru.job4j.calculate;
+/**
+ * Класс выводит строку в консоль.
+ *
+ * @author Egor Soroka ( https://vk.com/id428714363) 
+ */
 
 public class Calculate {
+	/**
+	* main
+	* @param args - args
+	* 
+	*/
+
 	public static void main (String [] args){
+		/**
+		* @return выводит в консоль "Hello World!!"
+		*/
 		System.out.println ("Hello World!!");
 	}
 }
