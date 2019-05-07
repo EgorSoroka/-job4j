cbad0a0c271ab3caca8cab806c2f1cafbb4b7517
George Magpie <prich845@gmail.com>
2019-05-07 15:06:01 +0300

    StartUi002 Почти

--- chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class StartUI {
     /**
      * Константа меню для добавления новой заявки.
@@ -22,9 +24,9 @@ public class StartUI {
     private static final String Delete = "3";
 
     /**
-     * Константа для полученя ID заявки.
+     * Константа для полученя заявки по ID.
      */
-    private static final String FindID = "4";
+    private static final String FindByID = "4";
 
     /**
      * Константа для получения имени заявки.
@@ -68,19 +70,15 @@ public class StartUI {
             if (ADD.equals(answer)) {
                 this.createItem();
             } else if (Show.equals(answer)){
-                tracker.findAll();
+                this.showItem();
             } else if (Edit.equals(answer)) {
-
-                tracker.replace()
+                this.editItem();
             } else if (Delete.equals(answer)){
-                tracker.delete()
-
-            } else if (FindID.equals(answer)){
-                tracker.findById()
-
+               this.deleteItem();
+            } else if (FindByID.equals(answer)){
+               this.findByID();
             } else if (FindName.equals(answer)) {
-                tracker.findByName()
-
+                this.findByName();
             } else if (EXIT.equals(answer)) {
                 exit = true;
             }
@@ -95,18 +93,72 @@ public class StartUI {
         System.out.println("------------ Добавление новой заявки --------------");
         String name = this.input.ask("Введите имя заявки :");
         String desc = this.input.ask("Введите описание заявки :");
-        Item item = new Item(name, desc);
+        long time = System.currentTimeMillis();
+        Item item = new Item(name, desc, time);
         this.tracker.add(item);
         System.out.println("------------ Новая заявка с getId : " + item.getId() + "-----------");
     }
 
+    /**
+     * Метод выводит список всех заявок.
+     */
+    private void showItem() {
+        System.out.println(Arrays.toString(this.tracker.findAll()));
+    }
+
+    /**
+     * Метод редактирует заявку.
+     */
+    private void editItem(){
+        System.out.println("   Редактирование заявки по ID   ");
+        String id = this.input.ask("Введите ID заявки");
+        this.tracker.replace(id); // как-то передать массив items
+        System.out.println("   Редактирование заявки   ");
+        String name = this.input.ask("Введите имя заявки :");
+        String desc = this.input.ask("Введите описание заявки :");
+        long time = System.currentTimeMillis();
+        Item item = new Item(name, desc, time);
+        this.tracker.add(item);
+        System.out.println("   Ваша заявка с getId : " + item.getId() + "   ");
+    }
+
+    /**
+     * Метод удаляет заявку.
+     */
+    private void deleteItem(){
+        System.out.println("   Удаление заявки   ");
+        String delete = this.input.ask("   Введите ID заявки   ");
+        this.tracker.delete(delete);
+    }
+
+    /**
+     * Метод ищет заявку по ID.
+     */
+    private void findByID(){
+        System.out.println("Поиск по ID");
+        String id = this.input.ask("   Введите ID заявки   ");
+        System.out.println(this.tracker.findById(id));
+    }
+
+    /**
+     * Метод ищет заявку по имени.
+     */
+    private void findByName() {
+        System.out.println("   Поиск заявки по имени   ");
+        String name = this.input.ask("   Введите имя заявки   ");
+        System.out.println(Arrays.toString(this.tracker.findByName(name)));
+    }
+
+    /**
+     * Метод отображает меню.
+     */
     private void showMenu() {
         System.out.println("Меню.");
         System.out.println("0 - создать новую заявку");
         System.out.println("1 - показать все заявки");
         System.out.println("2 - изменить заявку");
         System.out.println("3 - удалить заявку");
-        System.out.println("4 - получить ID заявки");
+        System.out.println("4 - получить заявку по ID");
         System.out.println("5 - получить имя заявки" );
         System.out.println("6 - Выход");
     }
