4c3bee6d912725e70e08284f0ad12f0c27d88d7e
George Magpie <prich845@gmail.com>
2019-05-08 23:10:34 +0300

    4. Используя шаблон проектирования - стратегию[#120766]Пора спать.

---
+++ chapter_002/src/main/java/ru/job4j/figures/Figures.java
@@ -0,0 +1,15 @@
+package ru.job4j.figures;
+
+import java.awt.*;
+
+public class Figures {
+    private Shape shape;
+
+    public Figures(Shape shape){
+        this.shape = shape;
+    }
+
+    public String figure(){
+        return shape.;
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/figures/Paint.java
@@ -0,0 +1,7 @@
+package ru.job4j.figures;
+
+public class Paint {
+    public static void main(String[] args) {
+
+    }
+  }
