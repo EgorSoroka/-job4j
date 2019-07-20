d5418541ea074ce6328d5b0134c4b68e61e660a4
George Magpie <prich845@gmail.com>
2019-07-20 19:13:30 +0300

    Каркас шахматной доски[#120783] 003

--- chess/src/main/java/ru/job4j/chess/firuges/Board.java
+++ chess/src/main/java/ru/job4j/chess/firuges/Board.java
@@ -2,18 +2,21 @@ package ru.job4j.chess.firuges;
 
 import ru.job4j.chess.chessexception.*;
 
+import java.util.stream.IntStream;
+
 public class Board {
     /**
      *  Содержит в себе шахматные фигуры.
      */
-    Figure[] figures = new Figure[32];
+    private Figure[] figures = new Figure[32];
+    private int index;
 
     /**
      * добавляет фигуры на доску.
      * @param figure шахматные фигуры.
      */
     public void add(Figure figure) {
-
+        this.figures[index++] = figure;
     }
 
     /**
@@ -23,12 +26,21 @@ public class Board {
      * @return
      */
     boolean move(Cell source, Cell dest) throws ImpossibleMoveException, OccupiedWayException, FigureNotFoundException {
-        if () {
+        this.index = noFigures(source);
+        if (this.index == - 1) {
             throw new FigureNotFoundException("Фигуры не найдена.");
         } if () {
             throw new ImpossibleMoveException("Данная фигура так двигаться не может");
         } if (){
             throw new OutOfMemoryError("На пути фигуры есть препятствие.");
         }
+    }
+
+    private int noFigures(Cell source) {
+            return IntStream.range(0, this.figures.length - 1)
+                    .filter(i->this.figures[i] != null)
+                    .filter(i->this.figures[i].position().equals(source))
+                    .findFirst()
+                    .orElse(-1);
     }
 }
